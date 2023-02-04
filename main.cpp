1e779f783f5e9c1626321d4ac0cce04e55b5687e
Anton Khitrikov <antonxitrikov@gmail.com>
2023-02-04 21:39:16 +0300

    v1.0

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main ()
+{
+    user_cout_function();
+}
+
+void user_cout_function(void)
+{
+    std::cout << "testing v1.0" << std::endl;
+}
---
+++ main.h
@@ -0,0 +1,3 @@
+#include <iostream>
+
+void user_cout_function (void);
